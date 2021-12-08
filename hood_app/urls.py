41307fb47f0c2e0b83b41388c93d01e1df0f267a
Moses <moses.junior@student.moringaschool.com>
2021-12-08 15:37:10 +0300

    addition of a urls.py file

---
+++ hood_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from hood_app import views 
+
+urlpatterns = [
+    path('',views.hood_view,name='neighbourhoods'),
+    path('join_hood/',views.join_hood,name='join_hood'),
+    path('my_hood/',views.get_hood,name='my_hood'),
+    path('my_hood/<int:pk>',views.get_residents,name='get_residents'),
+    path('move_out/',views.move_out,name='move_out'),
+    path('business/',views.business_view,name='business'),
+    path('business/<int:pk>',views.get_businesses,name='get_business'),
+    path('search/<str:term>',views.search_business,name='search_business'),
+    path('occurence/<int:pk>',views.occurence_view,name="occurence"),
+]
